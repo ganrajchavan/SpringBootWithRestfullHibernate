8a1d6ac510c646e2df7f1db9c45a5f44d881bda7
ganrajchavan <ganeshchavan117@gmail.com>
2022-08-12 13:37:43 +0530

    Spring bootproject

---
+++ src/main/java/com/GaneshIT/SpringBootCrudWithRestfulUsingHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.GaneshIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudWithRestfulUsingHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudWithRestfulUsingHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GaneshIT/SpringBootCrudWithRestfulUsingHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GaneshIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudWithRestfulUsingHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
